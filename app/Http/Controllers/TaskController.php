352cfb960f2399de890ea11499d54f0cfbf10ef0
claudiogsn <claudiogsn2@gmail.com>
2023-04-17 17:43:17 -0300

    Final push

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Task;
 use Orion\Concerns\DisableAuthorization;
+use Orion\Concerns\DisablePagination;
 use Orion\Http\Controllers\Controller as OrionController;
 
 class TaskController extends OrionController
 {
     use DisableAuthorization;
+    use DisablePagination;
     protected $model = Task::class;
+
+
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Orion\Concerns\DisablePagination;
 
 class Task extends Model
 {
     use HasFactory;
+    use DisablePagination;
+    protected $perPage = 0;
 
     protected $fillable = [
         'id',
